2fffb1f617a88b8bd98053ce9f350cc9b896b051
JFernando12 <fire.jf2012@gmail.com>
2022-03-04 11:59:32 -0600

    Nuevo cupon de descuento

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -1,8 +1,19 @@
 function calcularDescuento(){
     var precioOriginal = document.getElementById("precioOriginal").value;
     var descuento = document.getElementById("descuento").value;
-
+    var resultado = document.getElementById("resultado");
     var precioRebajado = precioOriginal * ((100 - descuento)/100);
 
-    alert("El precio con descuento es: " + precioRebajado);
+    precioFinal = calcularCupon(precioRebajado);
+
+    resultado.innerHTML = 
+    "El precio Original es: " + precioOriginal + "<br>" +
+    "El precio con descuento es: " + precioRebajado + "<br>" +
+    "El precio con descuento + cupon de descuento es: " + precioFinal;
+}
+
+function calcularCupon(precioOriginal){
+    var descuento = document.getElementById("cupon").value;
+    var precioRebajado = precioOriginal * ((100 - descuento)/100);
+    return precioRebajado
 }
