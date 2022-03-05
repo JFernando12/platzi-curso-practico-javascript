d856a87b0f7606ac421a663214a0f74af2acd2a2
JFernando12 <fire.jf2012@gmail.com>
2022-03-05 13:41:09 -0600

    ay la llevo jeje

--- promedio/promedio.js
+++ promedio/promedio.js
@@ -1,10 +1,33 @@
 
 numeros = [
-    400,
     500,
-    200,
-    350
+    600,
+    350,
+    300,
+    1000,
+    800,
+    1600,
+    1
 ]
+
+function ordenarLista(lista){
+    var guardar;
+
+    for (j = 0; j < (lista.length - 1); j ++){
+        for (i = 0; i < (lista.length - 1); i++){
+            if (lista[i] > lista[i+1]){
+                guardar = lista[i];
+                lista[i] = lista[i+1];
+                lista[i+1] = guardar;
+            }
+        }
+    }
+    
+    console.log(lista)
+
+}
+
+ordenarLista(numeros)
 
 function calcularaPromedio(lista){
     var resultado = document.getElementById("resultado");
@@ -19,4 +42,3 @@ function calcularaPromedio(lista){
     resultado.innerHTML = "El promedio es: " + promedio;
 }
 
-calcularaPromedio(numeros);
