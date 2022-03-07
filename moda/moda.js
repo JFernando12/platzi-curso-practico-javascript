546aa2dda5e8f2c6a5adf09612939803e5bf099a
JFernando12 <fire.jf2012@gmail.com>
2022-03-07 12:56:25 -0600

    Actualizar

--- moda/moda.js
+++ moda/moda.js
@@ -1,30 +1,34 @@
 var lista = [1, 2, 3, 1, 2, 1, 2, 3, 2, 1, 3, 2, 4, 4, 4];
 
-var contadorLista = {};
+function calcularModa(lista){
+    var contadorLista = {};
 
-lista.map(function(elemento){
-    if (contadorLista[elemento]){
-        contadorLista[elemento] += 1;
-    }
-    else {
-        contadorLista[elemento] = 1;
-    }
-})
+    lista.map(function(elemento){
+        if (contadorLista[elemento]){
+            contadorLista[elemento] += 1;
+        }
+        else {
+            contadorLista[elemento] = 1;
+        }
+    })
+    
+    console.log(contadorLista);
+    
+    var arrayLista = Object.entries(contadorLista);
+    
+    console.log(arrayLista);
+    
+    var ordenaLista = arrayLista.sort((a, b) => a[1] - b[1]);
+    
+    var moda = ordenaLista.pop()[0];
+    
+    console.log(moda)
+    
+    var resultado = document.getElementById("resultado");
+    var array = document.getElementById("array");
+    
+    array.innerHTML = "La lista es: " + lista;
+    resultado.innerHTML = "La moda es: " + moda; 
+}
 
-console.log(contadorLista);
-
-var arrayLista = Object.entries(contadorLista);
-
-console.log(arrayLista);
-
-var ordenaLista = arrayLista.sort((a, b) => a[1] - b[1]);
-
-var moda = ordenaLista.pop()[0];
-
-console.log(moda)
-
-var resultado = document.getElementById("resultado");
-var array = document.getElementById("array");
-
-array.innerHTML = "La lista es: " + lista;
-resultado.innerHTML = "La moda es: " + moda; 
+calcularModa(lista)
